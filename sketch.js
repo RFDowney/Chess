751294c0ffba0eee3c1146856a8f1825f5eec6a3
RFDowney <98475240+RFDowney@users.noreply.github.com>
2022-04-27 12:27:19 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,35 @@
-function setup() {
-  createCanvas(400, 400);
-}
 
+let BK,BQ,BH,BR,BP,WK,WQ,WH,WR,WP;
+function preload() {
+  BK = loadImage("Pieces/BKing.png");
+  BQ = loadImage("Pieces/BQueen.png");
+  BH = loadImage("Pieces/BHorse.png");
+  BR = loadImage("Pieces/BRook.png");
+  BB=loadImage("Pieces/BBishop.png")
+  BP = loadImage("Pieces/BPawn.png");
+  WK = loadImage("Pieces/WKing.png");
+  WQ = loadImage("Pieces/WQueen.png");
+  WH = loadImage("Pieces/WHorse.png");
+  WB=loadImage("Pieces/WBishop.png")
+  WR = loadImage("Pieces/WRook.png");
+  WP = loadImage("Pieces/WPawn.png");
+  console.log("Done preload");
+}
+function setup() {
+  createCanvas(410, 410);
+}
 function draw() {
   background(220);
-for(let y=0; y<8; y++){
-  if(y%2==0){
+  for(let y=0; y<8; y++){
     for(let x=0; x<8; x++){
-      square(30+40*x,30+40*y,40);
-      if(x%2==0){
-      fill(0,0,0);
-        
+      if((x+y)%2==0){
+        fill('white');
+      } else {
+        fill('darkgrey'); 
       }
-    }
-    }
-  if(y%2!=0){
-    for(let x=0; x<8; x++){
-      square(30+40*x,30+40*y,40);
-      if(x%2!=0){
-      fill(30+40*x,30+40*y,40);  
-      }
-    }
-    } 
-}
+      square(5+50*x,5+50*y,50);
+    }    
+  
+  }
+begin();
 }
